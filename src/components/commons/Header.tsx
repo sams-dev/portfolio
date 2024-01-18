bf19139fba999d642d326e6c4b555232f651618a
sams-dev <samuel2905.sms@gmail.com>
2024-01-18 17:23:41 -0300

    padronização de fonts com next/font

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import {Roboto} from 'next/font/google';
 
+const roboto = Roboto ({
+  subsets: ['latin'],
+  weight: '500',
+});
 export const Header = () => {
     return (
-        <header>
+        <header className={roboto.className}>
           <Link href='/'>
             <Image src="/Sd.png" width={50} height={50} alt="icone da samsdev" />
           </Link>
--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,16 +1,21 @@
 import { ReactNode } from "react";
 import { Header } from "./Header";
+import { Inter } from "next/font/google";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
+const inter = Inter ({
+  subsets: ['latin'],
+  weight: '400'
+});
 export const Layout = ({children}: LayoutProps) => {
     return (
-      <>
+      <div className={inter.className}>
         <Header/>
         {children}
-      </>
+      </div>
   
     );
 }
