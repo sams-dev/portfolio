e72bc78a7a59f6c1ec5d0ef294b3f183fd55b24d
sams-dev <samuel2905.sms@gmail.com>
2024-01-24 18:46:06 -0300

    estilização da page 404, ajuste no text de contatos

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,13 +7,15 @@ const NotFound = () => {
       <Head>
         <title>pagina não encontrada | samsdev</title>
       </Head>
-      <div>
-        <h1>404</h1>
-        <div>
+      <div className="text-center mt-12 md:mt-24 space-y-8 md:space-y-16 px-8 md:px-32">
+        <h1 className="text-5xl md:text-7xl font-bold">404</h1>
+        <div className="text-xl">
           <p>Oops, não conseguimos encontrar essa pagina!</p>
           <span>Clique no botão abaixo prar ser redirecionado à pagina inical</span>
         </div>
-        <Link href="/">ir para pagina inicial</Link>
+        <br />
+        <br />
+        <Link className="p-2 bg-gray-800 text-white font-medium text-xl md:text-2xl w-fit rounded-lg transition-all hover:bg-opacity-80" href="/">ir para pagina inicial</Link>
       </div>
     </>
 
--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -10,34 +10,34 @@ const Contatos = () => {
       <div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-8 md:px-32">
         <h1 className="text-5xl md:text-7xl font-bold text-center">Contatos</h1>
         <ul className="table mx-auto space-y-4 md:space-y-8">
-        <li className="md:text-xl">
+          <li className="md:text-xl">
             <span className="font-semibold">LinkedIn</span>
             <div>
-              <a className="text-xs md:text-lg text-slate-300 underline truncate" href="https://www.linkedin.com/in/samuel-moreira2905/">https://www.linkedin.com/in/samuel-moreira2905/</a>
+              <a className="text-sm md:text-lg text-slate-300 underline" href="https://www.linkedin.com/in/samuel-moreira2905/">https://www.linkedin.com/in/samuel-moreira2905/</a>
             </div>
           </li>
           <li className="md:text-xl">
             <span className="font-semibold">GitHub</span>
             <div>
-              <a className="text-xs md:text-lg text-slate-300 underline truncate" href="https://github.com/sams-dev">https://github.com/sams-dev</a>
+              <a className="text-sm md:text-lg text-slate-300 underline" href="https://github.com/sams-dev">https://github.com/sams-dev</a>
             </div>
           </li>
           <li className="md:text-xl">
             <span className="font-semibold">E-mail</span>
             <div>
-              <a className="text-xs md:text-lg text-slate-300 underline truncate" href="mailto:samsdev.23@gmail.com">samsdev.23@gmail.com</a>
+              <a className="text-sm md:text-lg text-slate-300 underline" href="mailto:samsdev.23@gmail.com">samsdev.23@gmail.com</a>
             </div>
           </li>
           <li className="md:text-xl">
             <span className="font-semibold">Whats</span>
             <div>
-              <a  href="/.">51 99351-4232</a>
+              <a href="/.">51 99351-4232</a>
             </div>
           </li>
           <li>
-            <PDF/>
+            <PDF />
           </li>
-          
+
         </ul>
       </div>
     </>
